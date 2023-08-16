084511844d1fa25ed0fdcafc5c3e4b17868a9e1f
Alpana Kushwaha <alpana95kush@gmail.com>
2023-08-17 05:25:28 +0530

    ErrorMessage Component

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const initialValues = {
@@ -20,7 +20,7 @@ const validationSchema = Yup.object({
 
 function YoutubeForm() {
   // console.log("Form errors", formik.errors);
-  console.log("Visited Fields", formik.touched);
+  // console.log("Visited Fields", formik.touched);
 
   return (
     <Formik
@@ -32,25 +32,18 @@ function YoutubeForm() {
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-
-          {formik.touched.name && formik.errors.name ? (
-            <div className="error">{formik.errors.name}</div>
-          ) : null}
+          <ErrorMessage name="name" />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
           <Field type="text" id="email" name="email" />
-          {formik.touched.email && formik.errors.email ? (
-            <div className="error">{formik.errors.email}</div>
-          ) : null}
+          <ErrorMessage name="email" />
         </div>
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
           <Field type="text" id="channel" name="channel" />
-          {formik.touched.channel && formik.errors.channel ? (
-            <div className="error">{formik.errors.channel}</div>
-          ) : null}
+          <ErrorMessage name="channel" />
         </div>
         <button type="submit">Submit</button>
       </Form>
